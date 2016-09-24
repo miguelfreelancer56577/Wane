e13f617ada6ef402c3733c3e5f2c70cf32ee7454
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2016-09-23 21:48:45 -0500

    On branch feature/activityStatusRepartidor: Se agrega actividad a androidmanifest Se crea actividad y layout inicial para el apartado del estatus del repartidor

---
+++ app/src/main/java/app/wane/com/wane/StatusMessenger.java
@@ -0,0 +1,13 @@
+package app.wane.com.wane;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StatusMessenger extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_messenger);
+    }
+}
